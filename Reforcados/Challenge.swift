2579c77d684d889c2dac80e6dbfc777406d2e19f
LuizGuerra <luizpedrofg@gmail.com>
2019-08-09 16:20:37 -0300

    challenge, challengeDAO, Screen Challenges, Unfinished resolution questions screen(model issue),

--- Reforcados/Challenge.swift
+++ Reforcados/Challenge.swift
@@ -13,11 +13,12 @@ class Challenge {
     let name: String
     let dueDate: Date
     let description: String
+    let questions: [Question]
     
-    
-    init(name: String, dueDate: Date , description: String) {
+    init(name: String, dueDate: Date , description: String, questions: [Question]) {
         self.name = name
         self.dueDate = dueDate
         self.description = description
+        self.questions = questions
     }
 }
--- Reforcados/ChallengeItemTableViewCell.swift
+++ Reforcados/ChallengeItemTableViewCell.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ChallengeItemTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+    
+   
+    @IBOutlet weak var challengeLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var dueDateLabel: UILabel!
+    
+    
 }
